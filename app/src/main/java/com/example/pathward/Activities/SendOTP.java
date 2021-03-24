8a1686c367bb5de450fa228b33a620bf2df09843
DELL <guptatanisha229@gmail.com>
2021-03-24 22:53:11 +0530

    Hii

--- app/src/main/java/com/example/pathward/Activities/SendOTP.java
+++ app/src/main/java/com/example/pathward/Activities/SendOTP.java
@@ -2,15 +2,28 @@ package com.example.pathward.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.pathward.R;
 
 public class SendOTP extends AppCompatActivity {
+    Button sendOtp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_otp);
+        sendOtp = findViewById(R.id.sendOtp);
+        sendOtp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SendOTP.this,Navigation_drawer.class));
+            }
+        });
+
+
     }
 }
